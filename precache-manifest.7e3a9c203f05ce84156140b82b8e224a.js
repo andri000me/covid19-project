597fca7bbb05306a6f643735d5769c1095bdac6b
dicky <dicky.factur07@gmail.com>
2020-04-11 22:50:33 +0700

    Updates

---
+++ precache-manifest.7e3a9c203f05ce84156140b82b8e224a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f8e650fa6feb8f334ec96f375f45086",
+    "url": "/covid19-project/index.html"
+  },
+  {
+    "revision": "4a2969c8ae5310524327",
+    "url": "/covid19-project/static/css/main.d7f68876.chunk.css"
+  },
+  {
+    "revision": "f5eda1fb7806995171a5",
+    "url": "/covid19-project/static/js/2.e97f9b23.chunk.js"
+  },
+  {
+    "revision": "2c24acf63e18487b21e343b8335b2718",
+    "url": "/covid19-project/static/js/2.e97f9b23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a2969c8ae5310524327",
+    "url": "/covid19-project/static/js/main.b099c258.chunk.js"
+  },
+  {
+    "revision": "299a2f811b2e44feb82e",
+    "url": "/covid19-project/static/js/runtime-main.11739167.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-project/static/media/covid.d7265326.png"
+  }
+]);
